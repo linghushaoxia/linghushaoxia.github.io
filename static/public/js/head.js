59a7c35a185a2d52c505a0054cd7a2bd7d8eb960
令狐少侠 <linghushaoxialove@163.com>
2017-05-07 19:42:09 +0800

    代码高亮

---
+++ static/public/js/head.js
@@ -0,0 +1,17 @@
+document.writeln("<!DOCTYPE html PUBLIC \'-//W3C//DTD HTML 4.01 Transitional//EN\' \'http://www.w3.org/TR/html4/loose.dtd\'>");
+document.writeln("<html>");
+document.writeln("<head>");
+document.writeln("		<meta http-equiv=\'Content-Type\' content=\'text/html; charset=UTF-8\'>");
+document.writeln("		<meta name=\'keywords\' content=\'html5, html 5, video, audio, html5video, html 5 video, html 5 audio, flash, h.264, h264, mp4, mp3, wav, aac, web, internet\'/>");
+document.writeln("		<meta name=\'description\' content=\'The html5media project enables HTML5 video and audio tags in all major browsers. All it takes is a single line of code.\'/>");
+document.writeln("		<link rel=\'stylesheet\' href=\'static/templates/hacker/css/style.css\'>");
+document.writeln("		<script type=\'text/javascript\' src=\'static/animation/mouseStar/js/mymouse.js\' id=\'mymouse\'></script>");
+document.writeln("	</head>");
+document.writeln("<body>");
+document.writeln("<header>");
+document.writeln("	<div class=\'container\'>");
+document.writeln("	<h1>星辰大海</h1>");
+document.writeln("	</div>");
+document.writeln("	</header>");
+document.writeln("</body>");
+document.writeln("</html>");
